9ab9ad25a844772ac26a0dafbcd61475c640cafc
miyaro <gibfenhddm386@gmail.com>
2020-06-02 19:28:39 +0900

    add second class project

---
+++ UniteSpice02/Assets/Scripts/SphereMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 idou = new Vector3(2f, 0f, 2f);
+        idou.x += 1f;
+        idou.y += 1f;
+
+        gameObject.transform.position = idou;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
