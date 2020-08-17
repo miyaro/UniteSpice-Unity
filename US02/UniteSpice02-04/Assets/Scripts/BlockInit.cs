2e556b31adb5c214a31f74792c8305a82aacc8bb
miyaro <gibfenhddm386@gmail.com>
2020-08-17 16:48:33 +0900

    add 03 and 04

---
+++ US02/UniteSpice02-04/Assets/Scripts/BlockInit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInit : MonoBehaviour
+{
+    //ブロックの元プレファブ
+    public GameObject blockResource;
+
+    //親オブジェクトの場所
+    public Transform blockParent;
+
+    public static int blockX = 11;
+    public static int blockY = 5;
+    public static int blockNum = blockX * blockY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < blockX; x++)
+        {
+            for (int y = 0; y < blockY; y++)
+            {
+                GameObject block = Instantiate(blockResource, blockParent);
+                block.transform.position = new Vector3(-12.5f + 2.5f * x, 7.25f - 0.8f * y, 10);
+            }
+        }
+    }
+
+}
---
+++ US02/UniteSpice02-04/Assets/Scripts/BoardController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += transform.right * 0.3f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position -= transform.right * 0.3f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.position.x >= 12.5f)
+        {
+            transform.position = new Vector3(12.5f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -12.5f)
+        {
+            transform.position = new Vector3(-12.5f, transform.position.y, transform.position.z);
+        }
+    }
+}
